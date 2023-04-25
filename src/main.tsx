8b345d43949b12925dd738cbafa66aece341b76c
everywang <wz15979455211@163.com>
2023-04-25 23:20:31 +0800

    嵌套路由的使用

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { ErrorPage } from './components/errorPage'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <h1>Hello Router</h1>,
-    errorElement: <ErrorPage />
+    element: <h1>Hello Router<Outlet /></h1>,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <div>请选择 1 2 3</div>
+      },
+      {
+        path: '1',
+        element: <div>1</div>
+      },
+      {
+        path: '2',
+        element: <div>2</div>
+      },
+      {
+        path: '3',
+        element: <div>3</div>
+      }
+    ]
   }
 ])
 const div = document.getElementById('root') as HTMLElement
