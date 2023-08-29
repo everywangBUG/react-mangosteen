471b7c2e0ba3c7b83de0a7dfcb108b7e83cc2a89
王政 <everywangbug@gmail.com>
2023-08-29 20:56:56 +0800

    动画的两个细节-页面闪烁-动画过度

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,5 +1,5 @@
 import { animated, useTransition } from '@react-spring/web'
-import { type ReactNode, useRef } from 'react'
+import { type ReactNode, useRef, useState } from 'react'
 import { Link, useLocation, useOutlet } from 'react-router-dom'
 import logo from '../assets/images/logo.svg'
 
@@ -13,32 +13,41 @@ export const WelcomeLayout: React.FC = () => {
     '/welcome/3': '/welcome/4',
     '/welcome/4': '/welcome/xxx',
   }
+  const [extraStyle, setExtraStyle] = useState({ position: 'relative' })
   map.current[location.pathname] = outlet
   const transitions = useTransition(location.pathname, {
     from: { transform: location.pathname === '/welcome/1' ? 'translate(0%)' : 'translateX(100%)' },
     enter: { transform: 'translateX(0%)' },
     leave: { transform: 'translateX(-100%)' },
-    config: { duration: 1000 }
+    config: { duration: 1000 },
+    onStart: () => {
+      setExtraStyle({ position: 'absolute' })
+    },
+    onRest: () => {
+      setExtraStyle({ position: 'relative' })
+    }
   })
-  return (<div flex flex-col items-stretch bg="#6335c3"
-               h-screen
-            >
-              <header shrink-0 text-center my-10px>
-                <img src={logo} w-65px/>
-                <h1 text="#dccff6" text-28px my-10px>山竹记账</h1>
-              </header>
-              <main grow-1 shrink-1 mx-16px>
-                {transitions((style, pathname) =>
-                    <animated.div key={pathname} style={style} w="100%" h="100%"
-                                  flex justify-center items-center bg="#ffffff" rounded-8px
-                    >
-                      {map.current[pathname]}
-                    </animated.div>
-                )}
-              </main>
-              <footer shrink-0 text-center mt-40px mb-24px grid grid-cols-3 grid-rows-1>
-                <Link style={{ gridArea: '1 / 2 / 2 / 3' }} text-28px text="#dccff6" to={linkMap[location.pathname]}>下一页</Link>
-                <Link style={{ gridArea: '1 / 3 / 2 / 4' }} text-28px text="#dccff6" to='/welcome/xxx'>跳过</Link>
-              </footer>
-            </div>)
+  return (
+    <div flex flex-col items-stretch bg="#6335c3"
+        h-screen
+    >
+      <header shrink-0 text-center my-10px mt-60px>
+        <img src={logo} w-65px h-70px/>
+        <h1 text="#dccff6" text-28px my-5px>山竹记账</h1>
+      </header>
+      <main grow-1 shrink-1 relative>
+        {transitions((style, pathname) =>
+            <animated.div key={pathname} style={{ ...style, ...extraStyle }} w="100%" h="100%">
+              <div flex justify-center items-center bg="#ffffff" rounded-8px mx-16px h="100%">
+                {map.current[pathname]}
+              </div>
+            </animated.div>
+        )}
+      </main>
+      <footer shrink-0 text-center mt-40px mb-24px grid grid-cols-3 grid-rows-1>
+        <Link style={{ gridArea: '1 / 2 / 2 / 3' }} text-28px text="#dccff6" to={linkMap[location.pathname]}>下一页</Link>
+        <Link style={{ gridArea: '1 / 3 / 2 / 4' }} text-28px text="#dccff6" to='/welcome/xxx'>跳过</Link>
+      </footer>
+    </div>
+  )
 }
