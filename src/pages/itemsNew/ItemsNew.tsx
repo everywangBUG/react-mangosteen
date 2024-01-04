d4ddf9b7253adfad0844000fa5e42b649417e578
王政 <everywangbug@gmail.com>
2024-01-04 15:24:11 +0800

    使用module.css改写styled-components深层样式

--- src/pages/itemsNew/ItemsNew.tsx
+++ src/pages/itemsNew/ItemsNew.tsx
@@ -1,20 +1,10 @@
 import { useState } from 'react'
 import type { ReactNode } from 'react'
-import styled from 'styled-components'
 import { Gradient } from '../../components/Gradient'
 import { TopNav } from '../../components/TopNav'
 import { Icon } from '../../components/Icon'
 import { Tabs } from '../../components/Tabs'
-
-const StyledTabs = styled(Tabs)`
-  .tabs-menu {}
-  .tabs-menu-item {
-    flex: 1;
-  }
-  .tabs-pane {
-    border: 1px solid red;
-  }
-`
+import s from './ItemsNew.module.scss'
 
 export type ExpendIncome = 'expenses' | 'income'
 
@@ -26,16 +16,16 @@ const itemsNewArr: { key: ExpendIncome; value: string; element: ReactNode }[] = 
 export const ItemsNew: React.FC = () => {
   const [tabItem, setTabItem] = useState<ExpendIncome>('expenses')
   return (
-    <>
+    <div className={s.wrapper}>
       <Gradient>
         <TopNav title='记一笔' icon={<Icon name="back" className="w-24px h-24px" />} />
       </Gradient>
-      <StyledTabs
+      <Tabs
         tabItems={itemsNewArr}
         selected={tabItem}
         onChange={(tabItem) => setTabItem(tabItem as ExpendIncome) }
         className='text-center' classPrefix="tabs"
       />
-    </>
+    </div>
   )
 }
