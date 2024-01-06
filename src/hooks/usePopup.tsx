a4ca6439e934a43a8b1eeaf12f83f476ef6f1f92
王政 <everywangbug@gmail.com>
2024-01-06 13:12:16 +0800

    使用createPortal传送门把popup挂载到body下，防止被popup父元素影响绝对定位

--- src/hooks/usePopup.tsx
+++ src/hooks/usePopup.tsx
@@ -1,9 +1,10 @@
+import ReactDOM from 'react-dom'
 import { useState } from 'react'
 import { Popup } from '../components/Popup'
 
 export const usePopup = () => {
   const [visible, setVisible] = useState(false)
-  const popup = <Popup visible={visible} onClickMask={() => setVisible(false)}/>
+  const popup = ReactDOM.createPortal(<Popup visible={visible} onClickMask={() => setVisible(false)}/>, document.body)
   return {
     popup,
     openPopup: () => setVisible(true),
