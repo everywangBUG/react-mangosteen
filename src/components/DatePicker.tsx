322fb1929bff40bd2e8d22ff87f0e407f3e0bc86
王政 <everywangbug@gmail.com>
2024-01-07 19:41:19 +0800

    一列变为三列的DatePicker

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,15 +1,26 @@
 import { useState } from 'react'
 import { time } from '../lib/time'
 
-type Props = {
+export const DatePicker: React.FC = () => {
+  return (
+    <div flex>
+      <Column className="grow-1" />
+      <Column className="grow-1" />
+      <Column className="grow-1"/>
+    </div>
+  )
+}
+
+type ColumnProps = {
   start?: Date
   end?: Date
   value?: Date
   itemHeight?: number
+  className?: string
 }
 
-export const DatePicker: React.FC<Props> = (props) => {
-  const { start, end, value, itemHeight = 36 } = props
+export const Column: React.FC<ColumnProps> = (props) => {
+  const { start, end, value, itemHeight = 36, className } = props
   const startTime = start ? time(start) : time().add(-10, 'years')
   const endTime = end ? time(end) : time().add(10, 'years')
   const valueTime = value ? time(value) : time()
@@ -31,39 +42,40 @@ export const DatePicker: React.FC<Props> = (props) => {
 
   return (
     <div
-    overflow="hidden"
-    h="50vh"
-    relative
-    onTouchStart={(e) => {
-      setIsTouching(true)
-      setLastY(e.touches[0].clientY)
-    }}
-    onTouchMove={(e) => {
-      if (isTouching) {
-        const y = e.touches[0].clientY
-        const dy = y - lastY
-        setTranslateY(translateY + dy)
-        setLastY(y)
-      }
-    }}
-    onTouchEnd={() => {
-      const remainder = translateY % itemHeight
-      if (Math.abs(remainder) < 18) {
-        setTranslateY(translateY - remainder)
-      }
-      else {
-        setTranslateY(translateY - remainder + itemHeight * Math.sign(remainder))
-      }
-      setIsTouching(false)
-    }}
-  >
-    <div b-1 b-red absolute w-full top="50%" style={{ height: itemHeight, transform: `translateY(${-itemHeight / 2})` }} />
-    <div b-1 b-red absolute w-full top="50%" style={{ height: itemHeight, transform: `translateY(${-itemHeight / 2})` }} >
-      <ol flex flex-col text-center children-flex children-items-center children-justify-center
-        style={{ transform: `translateY(${translateY}px)` }}
-      >
-        {yearList.map(item => <li style={{ height: itemHeight }} key={item}>{item}</li>) }
-      </ol>
+      className={className}
+      overflow="hidden"
+      h="50vh"
+      relative
+      onTouchStart={(e) => {
+        setIsTouching(true)
+        setLastY(e.touches[0].clientY)
+      }}
+      onTouchMove={(e) => {
+        if (isTouching) {
+          const y = e.touches[0].clientY
+          const dy = y - lastY
+          setTranslateY(translateY + dy)
+          setLastY(y)
+        }
+      }}
+      onTouchEnd={() => {
+        const remainder = translateY % itemHeight
+        if (Math.abs(remainder) < 18) {
+          setTranslateY(translateY - remainder)
+        }
+        else {
+          setTranslateY(translateY - remainder + itemHeight * Math.sign(remainder))
+        }
+        setIsTouching(false)
+      }}
+    >
+      <div border-b-2px border-t-2px b="#eee" absolute w-full top="50%" style={{ height: itemHeight, transform: `translateY(${-itemHeight / 2})` }} />
+      <div border-b-2px border-t-2px b="#eee" absolute w-full top="50%" style={{ height: itemHeight, transform: `translateY(${-itemHeight / 2})` }} >
+        <ol flex flex-col text-center children-flex children-items-center children-justify-center
+          style={{ transform: `translateY(${translateY}px)` }}
+        >
+          {yearList.map(item => <li style={{ height: itemHeight }} key={item}>{item}</li>) }
+        </ol>
     </div>
   </div>
   )
