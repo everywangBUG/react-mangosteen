9c473f057e77f0d4b09958a05d6c918cb7ffb179
王政 <everywangbug@gmail.com>
2024-01-17 21:48:45 +0800

    增加axios拦截器

--- src/lib/ajax.ts
+++ src/lib/ajax.ts
@@ -1,8 +1,18 @@
+import type { AxiosRequestConfig } from 'axios'
 import axios from 'axios'
 
 axios.defaults.baseURL = isDev ? '/' : 'http://121.196.236.94:8080/api/v1'
 axios.defaults.headers.post['content-Type'] = 'applocation/json'
 axios.defaults.timeout = 10000
+
+axios.interceptors.request.use((config: AxiosRequestConfig<any>) => {
+  const jwt = localStorage.getItem('jwt') || ''
+  config.headers = config.headers || {}
+  if (config.headers && jwt) {
+    config.headers.Authorization = `Bearer  + ${jwt}`
+  }
+  return config
+})
 
 export const ajax = {
   get: <T>(path: string) => {
