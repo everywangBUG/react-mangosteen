d24b7e7fa550c937ff9d459fd9431ce0a5660af9
王政 <everywangbug@gmail.com>
2024-01-24 22:21:18 +0800

    使用setInterval替换setTimeout的循环导致的误差bug

--- src/components/SmsCodeInput.tsx
+++ src/components/SmsCodeInput.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 type Props = {
   value?: string
@@ -7,29 +7,44 @@ type Props = {
   request?: () => Promise<unknown>
 }
 
+const maxCount = 60
 export const SmsCodeInput: React.FC<Props> = (props) => {
   const { placeholder, onChange, value, request } = props
-  const [count, setCount] = useState(60)
-  const [started, setStarted] = useState(false)
+  const [count, setCount] = useState(maxCount)
+  const [started, setStarted] = useState<Date>()
+  const timer = useRef<number>()
+
+  const clearTimer = () => {
+    if (timer.current) {
+      window.clearInterval(timer.current)
+      timer.current = undefined
+    }
+  }
 
   useEffect(() => {
-    if (!started) { return }
-    if (count === -1) {
-      setCount(60)
-      setStarted(false)
-      return
+    if (started) {
+      timer.current = window.setInterval(() => {
+        const time = new Date()
+        const seconds = Math.round((time.getTime() - started.getTime()) / 1000)
+        if (maxCount - seconds <= 0) {
+          setStarted(undefined)
+        }
+        setCount(maxCount - seconds)
+      }, 1000)
     }
-    const timer = setTimeout(() => {
-      setCount(count - 1)
-    }, 1000)
-    return () => clearTimeout(timer)
+    else {
+      clearTimer()
+    }
+    return () => {
+      clearTimer()
+    }
   }, [started, count])
 
   const onClick = async () => {
     if (!request) { return }
     await request?.(() => {})
     // 开始倒计时
-    setStarted(true)
+    setStarted(new Date())
   }
   return (
     <div flex gap-x-16px justify-between>
