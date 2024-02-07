24b8e19e801d3339362cbfe7d88ac95769f3b63f
everywangBUG <15979455211@163.com>
2024-02-07 11:29:30 +0800

    items页面使用replace优化后退体验

--- src/pages/itemsError.tsx
+++ src/pages/itemsError.tsx
@@ -8,7 +8,7 @@ export const ItemsErrors: React.FC = () => {
     return <Navigate to='/sign_in' />
   }
   else if (e instanceof ErrorEmptyData) {
-    return <Navigate to='/home' />
+    return <Navigate replace to='/home' />
   }
   else {
     return <div>出错了</div>
