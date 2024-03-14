9bcd54a68d5afb2f0a18b52994b8548ede29b8db
王政 <everywangbug@gmail.com>
2024-03-14 15:54:42 +0800

    点击自定义时间出现弹窗

--- src/components/TopTimeBar.tsx
+++ src/components/TopTimeBar.tsx
@@ -1,3 +1,4 @@
+import { usePopup } from '../hooks/usePopup'
 import { Tabs } from './Tabs'
 
 export type TimeRange =
@@ -19,9 +20,22 @@ const defaultTimeRanges: { key: TimeRange; value: string }[] = [
   { key: 'customTime', value: '自定义时间' }
 ]
 export const TopTimeBar: React.FC<Props> = (props) => {
-  const { selected, onSelect, timeRanges = defaultTimeRanges } = props
+  const { selected, onSelect: _onSelect, timeRanges = defaultTimeRanges } = props
+  const onConfirm = () => {
+    _onSelect('customTime')
+  }
+  const { openPopup, closePopup, popup } = usePopup({ children: <div onClick={onConfirm}>弹窗</div>, position: 'center' })
+  const onSelect = (key: TimeRange) => {
+    if (key === 'customTime') {
+      // 弹窗
+      openPopup()
+    } else {
+      _onSelect(key)
+    }
+  }
   return (
     <>
+      {popup}
       <Tabs tabItems={timeRanges} selected={selected} onChange={onSelect} />
     </>
   )
