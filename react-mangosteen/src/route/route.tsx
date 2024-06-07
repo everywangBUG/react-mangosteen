5aed31e128c202d9e7f57da9846a88526be95d9e
王政 <everywangbug@gmail.com>
2024-06-07 16:56:16 +0800

    嵌套路由的使用

--- react-mangosteen/src/route/route.tsx
+++ react-mangosteen/src/route/route.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Outlet } from "react-router-dom";
 import { ErrorPage } from "../components/ErrorPage";
 
 export const router = createBrowserRouter([
@@ -7,4 +7,29 @@ export const router = createBrowserRouter([
     element: (<div>home</div>),
     errorElement: <ErrorPage />
   },
+  {
+    path: "/welcome",
+    errorElement: <ErrorPage />,
+    element: <div>welcome<Outlet /></div>,
+    children: [
+      {
+        path: '1',
+        element: <div>1</div>,
+      },
+      {
+        path: '2',
+        element: <div>2</div>,
+      },
+      {
+        path: '3',
+        element: <div>3</div>,
+      },
+      {
+        path: '4',
+        element: <div>4</div>,
+      },
+      {
+      }
+    ]
+  }
 ]) 
