0045a4ea92d8a9cb2495af2ff32b5e25e909314a
王政 <everywangbug@gmail.com>
2024-06-07 15:02:38 +0800

    添加ErrorPage

---
+++ react-mangosteen/src/components/ErrorPage.tsx
@@ -0,0 +1,12 @@
+import { useRouteError } from "react-router-dom"
+
+export const ErrorPage: React.FC = () => {
+  const error: any = useRouteError()
+  console.log(error, 'placeholder')
+  
+  return (<div>
+    <h1>Oops!</h1>
+    <p>Sorry, an unexpected error has occurred.</p>
+    <p>{error.statusText || error.message}</p>
+  </div>)
+}
--- react-mangosteen/src/route/route.tsx
+++ react-mangosteen/src/route/route.tsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import { ErrorPage } from "../components/ErrorPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: (<div>home</div>)
-  }
-])
+    element: (<div>home</div>),
+    errorElement: <ErrorPage />
+  },
+]) 
