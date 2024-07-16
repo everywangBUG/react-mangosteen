90f4351ffe35929a5eb9c48d1c9035067eecb4b7
王政 <everywangbug@gmail.com>
2024-07-16 16:24:37 +0800

    add svgsprites plugin

--- react-mangosteen/vite.config.ts
+++ react-mangosteen/vite.config.ts
@@ -1,8 +1,13 @@
 import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { svgsprites } from './vite_plugins/svgsprites';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), UnoCSS()],
+  plugins: [
+    react(),
+    UnoCSS(),
+    svgsprites()
+  ],
 })
