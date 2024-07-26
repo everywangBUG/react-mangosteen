567ae148ee0b8a015b903cc6cb7c3d618c8618e6
王政 <everywangbug@gmail.com>
2024-07-26 14:33:42 +0800

    items page 1

--- react-mangosteen/src/views/Items.tsx
+++ react-mangosteen/src/views/Items.tsx
@@ -2,7 +2,7 @@ import { Icon } from "../components/Icon"
 import { TopNav } from "../components/TopNav"
 import { GradientTopNav } from "../components/Gradient"
 import { GradientTimeSelect } from "../components/Gradient"
-import "./items.module.scss"
+import s from "./items.module.scss"
 
 export const Items: React.FC = () => {
   return (
@@ -11,8 +11,8 @@ export const Items: React.FC = () => {
         <TopNav icon={<Icon name="menu" className="w-24px h-24px" />} title="橙子记账" />
       </GradientTopNav>
       <GradientTimeSelect>
-        <ol flex py-8px children-mx-16px text-white>
-          <li className="active">本月</li>
+        <ol flex children-py-8px children-px-16px text-white>
+          <li className={s.active}>本月</li>
           <li>上月</li>
           <li>今年</li>
           <li>自定义时间</li>
