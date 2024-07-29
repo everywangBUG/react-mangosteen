57bf8b5af8c6dfabdfd459ba82d6b3eb21cd2c19
王政 <everywangbug@gmail.com>
2024-07-29 10:10:20 +0800

    ItemsSummary ItemsList ItemsFloat Button

--- react-mangosteen/src/views/Items.tsx
+++ react-mangosteen/src/views/Items.tsx
@@ -3,12 +3,37 @@ import { Icon } from "../components/Icon"
 import { TopNav } from "../components/TopNav"
 import { GradientTopNav } from "../components/Gradient"
 import { TopTimeSelectBar } from "../components/TopTimeSelectBar"
+import { ItemSummary } from "./items/itemsSummary"
+import { ItemsList } from "./items/ItemsList"
+import { AddFloatButton } from "../components/AddFloatButton"
 
 type TimeRange = "thisMonth" | "lastMonth" | "thisYear" | "custom"
 
 export const Items: React.FC = () => {
   const [outOfTime, setOutOfTime] = useState(false)
   const [timeRange, _setTimeRange] = useState<TimeRange>("thisMonth")
+  const [items] = useState([
+    {
+      "id": 756,
+      "user_id": 285,
+      "name": "Lab.",
+      "sign": "😡",
+      "deleted_at": null,
+      "created_at": "2023-09-13T16:50:04.675+08:00",
+      "updated_at": "2023-09-13T16:50:04.675+08:00",
+      "kind": "expenses"
+    },
+    {
+      "id": 755,
+      "user_id": 285,
+      "name": "Bla.",
+      "sign": "😡",
+      "deleted_at": null,
+      "created_at": "2023-09-13T16:50:04.672+08:00",
+      "updated_at": "2023-09-13T16:50:04.672+08:00",
+      "kind": "expenses"
+    },
+  ])
   
   const setTimeRange = (t: TimeRange) => {
     _setTimeRange(t)
@@ -20,5 +45,8 @@ export const Items: React.FC = () => {
         <TopNav icon={<Icon name="menu" className="w-24px h-24px" />} title="橙子记账" />
       </GradientTopNav>
       <TopTimeSelectBar onSelect={setTimeRange} selected={timeRange} />
+      <ItemSummary />
+      <ItemsList items={items}/>
+      <AddFloatButton />
     </div>) 
 }
---
+++ react-mangosteen/src/views/items/itemsSummary.tsx
@@ -0,0 +1,19 @@
+export const ItemSummary: React.FC = () => {
+  return (
+    <>
+      <div flex bg-orange-3 my-10px mx-10px h-72px rounded-8px justify-around items-center>
+        <div flex flex-col children-text-18px children-text-gray-1 text-center>
+          <span>111</span>
+          <span>收入</span>
+        </div>
+        <div flex flex-col children-text-18px children-text-gray-1 text-center>
+          <span>222</span>
+          <span>支出</span>
+        </div>
+        <div flex flex-col children-text-18px children-text-gray-1 text-center>
+          <span>333</span>
+          <span>合计</span>
+        </div>
+      </div>
+    </>)
+}
